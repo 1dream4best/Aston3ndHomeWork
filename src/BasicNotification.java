b137712120330f8b1058cd921e0d040d8789bce4
1dream4best <1dream4best@gmail.com>
2026-08-29 01:45:19 +0600

    Add files via upload

---
+++ src/BasicNotification.java
@@ -0,0 +1,7 @@
+public class BasicNotification implements Notification{
+    //Decorator
+    @Override
+    public void send(String message){
+        System.out.println("Надоедливое уведомление: " + message);
+    }
+}
